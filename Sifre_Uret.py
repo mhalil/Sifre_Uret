eb42233e52d397ef3776b3af3a7f7fe41abadf06
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-09-25 20:52:41 +0300

    Temel mantığa ait baslangic dosyası.

---
+++ Sifre_Uret.py
@@ -0,0 +1,6 @@
+sozluk = {"A" : ["@", "4"]}     # sözlüğe tüm harf ve sayılara karşılık gelen değerler eklenecek.
+
+
+girdi_metin = input("İsminizi ya da şifreye dönüştürmek istediğiniz metni yazın: ")
+girdi_uzunluk = int(input("Üretilmesini istediğiniz şifre uzunluğunu belirtin: "))
+
